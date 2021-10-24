4ee27132124c2d26d877768f8cc33aac7f84c9e6
tychoverstraete <tycho@tychoverstraete.be>
2021-10-24 11:09:01 +0200

    mongodb connect

--- api/app.js
+++ api/app.js
@@ -3,9 +3,11 @@ const dotenv = require('dotenv')
 const app = express()
 const cors = require('cors')
 const helmet = require('helmet')
+const mongoose = require('mongoose')
 dotenv.config()
 const port = process.env.PORT || 3000
 const url = process.env.URL || 'http://localhost'
+const localDb = 'mongodb://localhost/my_database'
 
 const whitelist = [process.env.FRONTEND_URL]
 const corsOptions = {
@@ -17,16 +19,25 @@ const corsOptions = {
     }
   },
 }
+console.log(`⌛ Connecting to database...`)
+mongoose
+  .connect(process.env.DB_CONNECT || localDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`🔌 Connected to database`)
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    // app.use(cors(corsOptions)) TODO: UNCOMMENT IN PRODUCTION
+    app.use(helmet())
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-// app.use(cors(corsOptions)) TODO: UNCOMMENT IN PRODUCTION
-app.use(helmet())
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at ${url}:${port}`)
-})
+    app.listen(port, () => {
+      console.log(`⚡ Server running at ${url}:${port}`)
+    })
+  })
+  .catch((err) => console.log(err))
