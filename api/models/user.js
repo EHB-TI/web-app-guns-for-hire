64e089082de954adc5c0cd4892855bf964702edf
tychoverstraete <tycho@tychoverstraete.be>
2021-11-22 17:27:57 +0100

    jwt authorization

--- api/models/user.js
+++ api/models/user.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   spotify: {
     id: { type: String, required: true },
     refreshToken: { type: String, required: true },
     currentlyPlayingTrack: {},
   },
+  role: { type: String, enum: ['watcher', 'streamer'], default: 'watcher' },
 })
 
 module.exports = mongoose.model('User', userSchema)
--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -5,6 +5,7 @@ const errorResponse = require('../helpers/error-response')
 const successResponse = require('../helpers/success-response')
 const passport = require('passport')
 const initializePassport = require('../config/passport.setup')
+const jwtService = require('../helpers/jwt.service')
 
 initializePassport(passport)
 
@@ -22,13 +23,14 @@ router.get(
   '/spotify/callback',
   passport.authenticate('spotify', { failureRedirect: '/login' }),
   function (req, res) {
-    res.redirect('/')
+    const token = jwtService.generateAccessToken(req.user)
+    res.status(200).json(successResponse(res.statusCode, { accessToken: token }))
   }
 )
 
-router.get('/logout', function (req, res) {
+router.get('/logout', jwtService.authenticateToken, function (req, res) {
   req.logout()
-  res.redirect('/')
+  res.status(200).json(successResponse(res.statusCode, 'You are logged out successfully'))
 })
 
 module.exports = router
